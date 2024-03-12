8c99e6c4c703f7122f26ffd1afec2e1cd37ee87e
Alexander Sunnerhage <alexsunnerhage@hotmail.com>
2024-03-12 10:47:57 +0000

    Add contact app for future collaborations

--- newsflash/urls.py
+++ newsflash/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from hello_news import views as home_views
 
 urlpatterns = [
+    path('', home_views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
