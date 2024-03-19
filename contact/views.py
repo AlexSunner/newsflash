f87f284e40445de30c3273cb0e042801048ca1ae
Alexander Sunnerhage <alexsunnerhage@hotmail.com>
2024-03-19 15:26:30 +0000

    Implement contact form for collaborations

--- contact/views.py
+++ contact/views.py
@@ -1,15 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Contact
+from .forms import CollaborateForm
 
-# Create your views here.
 def contact_me(request):
     """
     Renders the Contact page
     """
     contact = Contact.objects.all().order_by('-updated_on').first()
+    
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.success(request, "Collaboration request received!")
+            return redirect('contact')  # Redirect after successful form submission
+    else:
+        collaborate_form = CollaborateForm()
 
     return render(
         request,
         "contact/contact.html",
-        {"contact": contact},
+        {
+            "contact": contact,
+            "collaborate_form": collaborate_form
+        },
     )
